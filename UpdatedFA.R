0a6043fcea41fdc293a985bbcdfe7668f03b823a
choobs96 <37956681+choobs96@users.noreply.github.com>
2019-05-09 21:09:02 +0800

    Updated dataframe

--- UpdatedFA.R
+++ UpdatedFA.R
@@ -1,69 +1,80 @@
 library(shiny)
+library(DT)
 source("./function.R")
 
 ui <- fluidPage(
   titlePanel("Financial Calculator"),
   
   #sidebarLayout(
-    #sidebarPanel(
-      helpText("Help you calculate your expenses"),
-      
-      selectInput("var", 
-                  label = "Choose your expenses",
-                  choices = c("Petrol", 
-                              "Food",
-                              "Rental", 
-                              "Loan"),
-                  selected = "Food"),
-      numericInput("init_income",
-                   "Enter your initial income:",
-                   value = 3000),
-      numericInput("growth_rate",
-                   "Enter the expected income growth rate in %:",
-                   value = 5),
-      numericInput("growth_duration",
-                   "Duration needed for each increments in years:",
-                   value = 2),
-      numericInput("promo_rate",
-                   "Enter your increment rate for promotion:",
-                   value = 10),
-      numericInput("promo_time",
-                   "Duration for promotion:",
-                   value = 5),
-      numericInput("numInput",
-                   "Enter your yearly saving at the end of the month
-                   in RM:",
-                   value = 1000),
+  #sidebarPanel(
+  helpText("Help you calculate your expenses"),
   
-      numericInput("AIR",
-                   "Enter the current interest rate in %:",
-                   value = 5),
-      sliderInput("range", 
-                 label = "Years of Saving:",
-               min = 1, max = 50, value = 10),
-      hr(),
-      actionButton("action","Saving Projection"),
-      
-    #),
-    
-    #mainPanel(
-    textOutput("selected_var"),
-    textOutput("txtOutput"),
-    textOutput("AIRout"),
-    textOutput("FV"),
-    textOutput("init_income_out"),
-    textOutput("growth_rate_out"),
-    textOutput("duration_out"),
-    plotOutput("plot_saving"),
-    plotOutput("plot_income")
-      
-    #)
+  selectInput("var", 
+              label = "Choose your expenses",
+              choices = c("Petrol", 
+                          "Food",
+                          "Rental", 
+                          "Loan"),
+              selected = "Food"),
+  numericInput("age",
+               "Input your current age:",
+               value = 20),
+  selectInput("age_m", 
+              label = "How many months old? 0 represents 12",
+              choices = c(0:11),
+              selected = 0),
+    numericInput("init_income",
+               "Enter your initial income:",
+               value = 3000),
+  numericInput("growth_rate",
+               "Enter the expected income growth rate in %:",
+               value = 5),
+  numericInput("growth_duration",
+               "Duration needed for each increments in years:",
+               value = 2),
+  numericInput("promo_rate",
+               "Enter your increment rate for promotion:",
+               value = 10),
+  numericInput("promo_time",
+               "Duration for promotion:",
+               value = 5),
+  numericInput("numInput",
+               "Enter your yearly saving at the end of the month
+               in RM:",
+               value = 1000),
+  
+  numericInput("AIR",
+               "Enter the current interest rate in %:",
+               value = 5),
+  sliderInput("range", 
+              label = "Years of Saving:",
+              min = 1, max = 50, value = 10),
+  DT::dataTableOutput("mytable"),
+  hr(),
+  actionButton("action","Saving Projection"),
+  
+  
+  #),
+  
+  #mainPanel(
+  textOutput("selected_var"),
+  textOutput("txtOutput"),
+  textOutput("AIRout"),
+  textOutput("FV"),
+  textOutput("init_income_out"),
+  textOutput("growth_rate_out"),
+  textOutput("duration_out"),
+  plotOutput("plot_saving"),
+  plotOutput("plot_income")
+  
   #)
-#)
+  #)
+  #)
 )
 
 server <- function(input, output) {
 
+  
   output$selected_var <- renderText({ 
     paste("You have selected", input$var)
   })
@@ -71,7 +82,7 @@ server <- function(input, output) {
     paste("Your yearly saving is RM: ", input$numInput)
   })
   output$FV <- renderText({
-  paste("Your future value for",input$range,"years is RM: ", as.numeric(future_val(input$numInput,input$AIR,input$range)))
+    paste("Your future value for",input$range,"years is RM: ", as.numeric(future_val(input$numInput,input$AIR,input$range)))
   })
   output$AIRout <- renderText({
     paste("Your annual rate of return is: ", input$AIR,"%")
@@ -91,10 +102,13 @@ server <- function(input, output) {
     v$incomedata <- income_proj(input$init_income,input$growth_rate,input$growth_duration,input$range)
     #input$init_income,input$growth_rate,input$growth_duration,input$range
   })
-  
+
+  output$mytable = DT::renderDataTable({
+    final_df(as.numeric(input$age),as.numeric(input$range),as.numeric(input$age_m),as.numeric(input$growth_rate),as.numeric(input$growth_duration),as.numeric(input$promo_rate),as.numeric(input$promo_time))
+  })
   output$plot_saving <- renderPlot({
     if (is.null(v$data)) return()
-  plot(x=c(0:(length(v$data)-1)),y=v$data,type='l',ylab='Savings in RM',xlab='Year',main=paste('Projection of Savings over',input$range,' years'))
+    plot(x=c(0:(length(v$data)-1)),y=v$data,type='l',ylab='Savings in RM',xlab='Year',main=paste('Projection of Savings over',input$range,' years'))
   })
   output$plot_income <- renderPlot({
     if (is.null(v$incomedata)) return()
@@ -104,4 +118,3 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
-
--- function.R
+++ function.R
@@ -3,13 +3,13 @@ future_val<-function(p,r,n){
   
 }
 saving_proj<-function(p,r,n){
-data<-rep(0,n+1)
-init=0
-for (i in 1:n){
-  init=init+future_val(p,r,i)
-  data[i+1]<-init
+  data<-rep(0,n+1)
+  init=0
+  for (i in 1:n){
+    init=init+future_val(p,r,i)
+    data[i+1]<-init
   }
-return(data)
+  return(data)
 }
 #plot(data,type='l')
 #saving_proj(1000,5,30)
@@ -17,18 +17,46 @@ return(data)
 #income_proj(3000,5,2,5)
 income_proj<-function(income,r,year,n,pr,p){
   if(missing(p) & missing(pr)){
-  data<-rep(0,(n%/%year)+1)
-  init=income
-  data[1]<-income
-  for (i in 1:(n%/%year)){
-    init=init*(1+(r/100))
-    data[i+1]<-init
-  }
-  return(data)
+    data<-rep(0,(n%/%year)+1)
+    init=income
+    data[1]<-income
+    for (i in 1:(n%/%year)){
+      init=init*(1+(r/100))
+      data[i+1]<-init
+    }
+    return(data)
   }
   else{
     
   }
 }
 income_proj(3000,5,2,10)
+final_df<-function(age,n,month,ir,growth_d,pr,pt){
+  age_c<-c(age:(age+n))
+  age_ot<-c(rep(age_c[1],times=(12-month)),rep(age_c[2:length(age_c)],rep(12,(length(age_c)-1))))
+  age_ot<-age_ot[1:(n*12)]
+  month_ot<- c(month:11,rep(c(0:11),(length(age_c)-1)))
+  month_ot<-month_ot[1:(n*12)]
+  #print(age_ot)
+  #print(month_ot)
+  ####
+  growth_r<-rep(0,12*n)
+  temp<-n%/%growth_d
+  temp_1<-c(1:temp)
+  temp_2<-temp_1*12*growth_d
+  growth_r[temp_2]<-ir
+  #print(length(growth_r))
+  ##
+  promo_r<-rep(0,12*n)
+  temp_p<-n%/%pt
+  temp_p_1<-c(1:temp_p)
+  temp_p_2<-temp_p_1*12*pt
+  promo_r[temp_p_2]<-pr
+  ##
+
+  ##
+  age_df<-data.frame(age_ot,month_ot,growth_r,promo_r)
+  return(age_df)
+}
+
 

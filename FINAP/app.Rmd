fe23b6cd8583338e302e2e6c4798a51389440970
yinyen <>
2019-05-25 13:24:41 +0800

    slides

---
+++ FINAP/app.Rmd
@@ -0,0 +1,69 @@
+---
+date: "25 May 2019"
+output: ioslides_presentation
+title       : "FiNap"
+subtitle    : "An App for Financial and Retirement Planning"
+author1     : "Cheah Jun Yitt    WQD180107"
+author2     : "Choo Jian Wei     WQD180124"
+author3     : "Tan Yin Yen       WQD180108"
+# framework   : io2012    
+# highlighter : highlight.js  
+# hitheme     : tomorrow      
+# widgets     : [bootstrap]
+mode        : selfcontained
+# knit        : slidify::knit2slides
+---
+
+
+## Data Story
+<style>
+.redd { font-color: #934141 }
+</style>
+
+<style type='text/css'>
+img {
+    max-height: 390px;
+    max-width: 1000px;
+}
+</style>
+
+
+<style>
+strong {
+  font-weight: bold;
+}
+</style>
+
+<style type="text/css">
+    body {background:grey transparent;
+    }
+</style>
+
+James is a 28-year-old accountant that earns <span class='green3'>RM 7000 monthly</span>. He spends all her salary on <span class='red3'>luxuries</span> and maxes out on <span class='red3'>credit cards</span>. He even takes up loan to finance <span class='red3'>overseas trip, luxurious car and expensive house</span>. One day, his mother is diagnosed with <span class='red3'>cancer</span> and needs a huge amount of medical expenses. <span class='yellow'>Without any savings or investment</span>, can he get by his <span class='red3'>current situation</span>>? Can he even plan for his <span class='blue3'>retirement</span>?
+  
+<div style='text-align: center;'>
+</div>
+
+----  
+
+## Overview
+<span class='red3'>FiNap</span> is the solution for <span class='red3'>young working adults</span> like James. By planning ahead of time, young working adults can manage their personal finance, have ample savings for <span class='red3'>emergency situations</span> and plan for a <span class='red3'>prosperous retirement life</span>.
+  
+<div style='text-align: center;'>
+</div>
+
+----
+## App Input
+
+FiNap allows users to input data like <span class='red3'>income, saving, investment, expense, house loan, car loan</span> and <span class='red3'>child education cost</span>.
+  
+
+----
+## Projection
+
+FiNap <span class='red3'>projects</span> users' <span class='red3'>future income</span> and <span class='red3'>expenses</span> which allow users to see their <span class='red3'>net worth at the age of retirement</span>.
+  
+<div style='text-align: center;'>
+</div>
+  
+Finally, let's look at the demonstration of the app [here](https://jycheah.shinyapps.io/FiNap). 
--- UI_Overview.R
+++ UI_Overview.R
@@ -10,9 +10,9 @@ library(DT)
 oTitle <- fluidRow(
     column(width = 12,
            tags$div(
-               HTML(paste(tags$span(style="font-weight:bold; color:#12454f", "FiNap"))), style = "font-size:1.7em"),
+               HTML(paste(tags$span(style="font-weight:bold; color:#12454f", "FiNap"))), style = "font-size:1.8em",align = "center"),
            tags$div(
-               HTML(paste(tags$span(style="color:#1c5a66", "- Nap More, Worry Less -"))), style = "font-size:1.4em")
+               HTML(paste(tags$span(style="color:#1c5a66", "- Nap More, Worry Less -"))), style = "font-size:1.4em",align = "center")
     ))
 
 oInfo <- fluidRow(
@@ -21,7 +21,7 @@ oInfo <- fluidRow(
                HTML(paste("FiNap is a ", 
                           tags$span(style="font-weight:bold; color:#337a87", "financial planning app"),
                           " designed mainly for young working adults to understand their financial status and plan their retirement ahead.",
-                          sep = "")), style = "font-size:1.1em"
+                          sep = "")), style = "font-size:1.2em"
            ),hr(style = "border-color:white;margin-bottom:0.25em"),
            tags$div(
                HTML(paste("It allows users to input data like ", 
@@ -33,17 +33,17 @@ oInfo <- fluidRow(
                           " and ", 
                           tags$span(style="font-weight:bold; color:#337a87", " child education cost"),
                           ". It then does all the calculation and forecasting based on the input and assumptions.",
-                          sep = "")), style = "font-size:1.1em"
+                          sep = "")), style = "font-size:1.2em"
            ),hr(style = "border-color:white;margin-bottom:0.25em"),
            tags$div(
                HTML(paste("Finally, it project users' future income and expenses allowing users to see their net worth at the age of retirement and whether at any point in their life, they are in debt."))
-               ,style = "font-size:1.1em"), 
+               ,style = "font-size:1.2em"), 
            hr(style = "border-color:white;margin-bottom:0.25em"), 
            tags$div(
                HTML(paste("By having a good financial planning ahead of time, young working adults will be able to ", 
                           tags$span(style="font-weight:bold; color:#337a87", "avoid the pitfall of being debt-ridden"),
                           " and have enough savings for a ",
                           tags$span(style="font-weight:bold; color:#337a87", "prosperous retirement life."),
-                          sep = "")), style = "font-size:1.1em"
+                          sep = "")), style = "font-size:1.2em"
            )
     ))
